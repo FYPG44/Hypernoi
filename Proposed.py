3b7786ddc9ce37f88ff37be592413c6a61c37c4e
Abhishek Pandey <70065781+Abhi-dev007@users.noreply.github.com>
2023-04-20 23:17:40 +0530

    Parallel reduction added
    Not properly working

--- Proposed.py
+++ Proposed.py
@@ -13,6 +13,8 @@ class proposed_solver_2D:
         self.num_site = sites.shape[0]
         self.pixels = ti.Vector.field(n=sites.shape[0], dtype=ti.i32, shape=(self.w, self.h))
         self.pixels.fill(10**9)
+        self.comp_result = ti.Vector.field(n=sites.shape[0], dtype=ti.i32, shape=(self.w, self.h))
+        self.comp_result.fill(-1)
         self.result = ti.field(dtype=ti.i32, shape=(self.w, self.h))
         self.result.fill(-1)
         self.sites = ti.Vector.field(n=sites.shape[1], dtype=ti.f32, shape=(sites.shape[0],))
@@ -72,7 +74,7 @@ class proposed_solver_2D:
 
     @ti.func
     def grow_circle(self, site_ind):
-        for r in range(self.radii[site_ind]):
+        for r in range(100):
             self.circle_dcs(r, site_ind)
 
     @ti.kernel
@@ -80,81 +82,65 @@ class proposed_solver_2D:
         for i in range(self.num_site):
             self.grow_circle(i)
     
-    @ti.func
-    def find_color(self, i, j):
-        radius, seed = 10**9, -1
-        for k in range(self.num_site):
-            if self.pixels[i, j][k] < radius:
-                radius = self.pixels[i, j][k]
-                seed = k
-        return seed
-        
+
     @ti.kernel
     def generate_result(self):
         for i, j in self.result:
-            self.result[i, j] = self.find_color(i, j)
+            for k in ti.static(range(6,-1,-1)):
+                ti.block_dim(2**k)
+                for l in ti.static(range(2**k)):
+                    if self.pixels[i, j][l] > self.pixels[i, j][l + 2**k]:
+                        self.pixels[i, j][l] = self.pixels[i, j][l + 2**k]
+                        self.comp_result[i, j][l] = l + 2**k
+                    elif self.comp_result[i, j][l] == -1 and self.pixels[i, j][l] != 10**9:
+                        self.comp_result[i, j][l] = l
+
 
     @ti.kernel
+    def minn(self):
+        for i, j in self.result:
+            self.result[i, j] = self.comp_result[i, j][0]
+
+#     @ti.func
+#     def find_color(self, i, j):
+#         radius, seed = 10**9, -1
+#         for k in range(self.num_site):
+#             if self.pixels[i, j][k] < radius:
+#                 radius = self.pixels[i, j][k]
+#                 seed = k
+#         return seed
+
+    # @ti.kernel
+    # def generate_result(self):
+    #     for i, j in self.result:
+    #         self.result[i, j] = self.find_color(i, j)
+
+
+    @ ti.kernel
     def render_color(self, screen: ti.template(), site_info: ti.template()):
         for I in ti.grouped(screen):
             if self.result[I] != -1:
                 screen[I] = site_info[self.result[I]]
             else:
                 screen[I].fill(-1)
+
     @ti.kernel
     def render_index(self, screen: ti.template()):
         for I in ti.grouped(screen):
-            if self.result[I] != -1:
-                screen[I].fill(self.result[I] / self.num_site)
+            if self.pixels[I] != -1:
+                screen[I].fill(self.pixels[I] / self.num_site)
             else:
                 screen[I].fill(-1)
-                    
+
+
     # @ti.kernel
     def display(self):
         plt.imshow(self.result.to_numpy(), interpolation='nearest')
         plt.show()
     
 
-    @ti.func
-    def find_avg_radii(self, i, j):
-        index = ti.cast(
-                ts.vec(self.sites[i].x*self.w, self.sites[i].y*self.h), ti.i32)
-        xc, yc = index.x, index.y
-
-        index = ti.cast(
-                ts.vec(self.sites[j].x*self.w, self.sites[j].y*self.h), ti.i32)
-        xc1, yc1 = index.x, index.y
-        self.radii[i] += max(abs(xc - xc1), abs(yc - yc1))
-
-
-
-    @ti.kernel
-    def find_optimum_radii(self):
-        for num in range(1, self.num_site_sqr+1):
-            j = num % self.num_site
-            if j == 0:
-                j = self.num_site
-            i = (num + self.num_site - 1) // self.num_site
-
-            self.find_avg_radii(i-1, j-1)
-
-
-    @ti.func
-    def find_avg_radii_final(self, i):
-        self.radii[i] //= 2*self.num_site
-        print(self.radii[i])
-        # self.radii[i] = 100
-
-
-    @ti.kernel
-    def find_optimum_radii_final(self):
-        for i in range(self.num_site):
-            self.find_avg_radii_final(i)
-
-
     # @ti.func
     def solve_proposed(self):
-        self.find_optimum_radii()
-        self.find_optimum_radii_final()
         self.fill_frames()
         self.generate_result()
+        self.minn()
