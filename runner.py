2e55063e793ca9936522870cfefe74d9ddf2cacc
Abhishek Pandey <70065781+Abhi-dev007@users.noreply.github.com>
2023-04-20 23:18:55 +0530

    minor changes in runner.py

--- runner.py
+++ runner.py
@@ -15,28 +15,28 @@ from matplotlib import pyplot as plt
 # ti.profiler.print_kernel_profiler_info()
 
 # runner for proposed algo
-# width, height, seeds, seeds_info = 512, 512, np.array(np.random.rand(100, 2), dtype=np.float32), np.array(np.random.rand(100, 3), dtype=np.float32)
-# screen = ti.Vector.field(n=3, dtype=ti.f32, shape=(width, height))
-# info = ti.Vector.field(n=3, dtype=ti.f32, shape=seeds_info.shape[0])
-# info.from_numpy(seeds_info)
-# voronoi = proposed_solver_2D(width, height, seeds)
-# voronoi.solve_proposed()
-# voronoi.render_color(screen, info)
-# plt.imshow(screen.to_numpy(), interpolation='nearest')
-# plt.show()
-# voronoi.display() # uncomment for obtaining image of Voronoi 
-# ti.profiler.print_kernel_profiler_info()
-
-# runner for proposed algo with sqrt decomp
-width, height, seeds, seeds_info = 512, 512, np.array(np.random.rand(100, 2), dtype=np.float32), np.array(np.random.rand(100, 3), dtype=np.float32)
+width, height, seeds, seeds_info = 512, 512, np.array(np.random.rand(128, 2), dtype=np.float32), np.array(np.random.rand(128, 3), dtype=np.float32)
 screen = ti.Vector.field(n=3, dtype=ti.f32, shape=(width, height))
 info = ti.Vector.field(n=3, dtype=ti.f32, shape=seeds_info.shape[0])
 info.from_numpy(seeds_info)
-voronoi = proposed_solver_2D_with_sqrt_decomp(width, height, seeds)
+voronoi = proposed_solver_2D(width, height, seeds)
 voronoi.solve_proposed()
 voronoi.render_color(screen, info)
 plt.imshow(screen.to_numpy(), interpolation='nearest')
 plt.show()
+# voronoi.display() # uncomment for obtaining image of Voronoi 
+ti.profiler.print_kernel_profiler_info()
+
+# runner for proposed algo with sqrt decomp
+# width, height, seeds, seeds_info = 512, 512, np.array(np.random.rand(100, 2), dtype=np.float32), np.array(np.random.rand(100, 3), dtype=np.float32)
+# screen = ti.Vector.field(n=3, dtype=ti.f32, shape=(width, height))
+# info = ti.Vector.field(n=3, dtype=ti.f32, shape=seeds_info.shape[0])
+# info.from_numpy(seeds_info)
+# voronoi = proposed_solver_2D_with_sqrt_decomp(width, height, seeds)
+# voronoi.solve_proposed()
+# voronoi.render_color(screen, info)
+# plt.imshow(screen.to_numpy(), interpolation='nearest')
+# plt.show()
 # voronoi.display() # uncomment for obtaining image of Voronoi 
 # ti.profiler.print_kernel_profiler_info()
 
